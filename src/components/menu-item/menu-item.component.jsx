09ae7c9e975004a08bec0ed4c698297504260d52
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-07 19:34:44 +0600

    broken our homepage into smaller components, finished styling our menu-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,14 +5,15 @@ import './manu-item.styles.scss';
 // we are using the function component because we don't want to need are hold any state
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{
-      backgroundImage: `url(${imageUrl})`,
-    }}
-    className={`${size} menu-item`}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
     <div className='content'>
-      <h1 className='title'>{title}</h1>
+      <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>

38c24077a1cb7f3c57166d6e6315b06320308929
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-07 19:09:50 +0600

    ecommerce website

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './manu-item.styles.scss';
+
+// we are using the function component because we don't want to need are hold any state
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+    }}
+    className={`${size} menu-item`}
+  >
+    <div className='content'>
+      <h1 className='title'>{title}</h1>
+      <span className='subtitle'>SHOP NOW</span>
+    </div>
+  </div>
+);
+
+MenuItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
+};
+
+export default MenuItem;

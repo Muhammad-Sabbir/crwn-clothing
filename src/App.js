96b29e4fd05b502947c55f3f2eedb557bdcf3a11
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-09 07:10:10 +0600

    Upto->Chapter:5. Master Project React Router and Routing>>Lecture: 2. Routing In Our Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 // import logo from "./logo.svg";
-// import React from 'react';
+import React from 'react';
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; // We are using new version of react-router-dom, so previous methodds are deprecated, new link https://reactrouter.com/en/main/start/tutorial#adding-a-router
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/hats',
+    element: <HatsPage />,
+  },
+]);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
   );
 }
